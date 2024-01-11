1020046e791de94d0a28fd80ffd2d6c5ea7036da
Illich Rada <illich570@gmail.com>
2024-01-10 23:33:06 -0400

    [ADD] added valibot and initial components for forms

---
+++ src/components/form/InputController.tsx
@@ -0,0 +1,39 @@
+import { InputHTMLAttributes } from 'react'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/Form'
+import { Input } from '@/components/ui/Input'
+import { useFormContext } from 'react-hook-form'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  label: string
+  description: string
+}
+
+const InputController = ({ name, label, description, ...propsField }: InputProps) => {
+  const { control } = useFormContext()
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input {...field} {...propsField} />
+          </FormControl>
+          {description && <FormDescription>This is your public display name.</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export { InputController }
