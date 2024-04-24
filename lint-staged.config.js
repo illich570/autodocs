040ab91cb5d08ff0c6470a9fadf4d1977719f571
Illich Rada <illich570@gmail.com>
2024-04-24 15:05:41 -0400

    [REF] format & lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 export default {
   // This will lint JavaScript files
   '**/*.(tsx|ts)': (filenames) => [
-    `yarn eslint --fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `pnpm eslint --fix ${filenames.join(' ')}`,
+    `pnpm prettier --write ${filenames.join(' ')}`,
   ],
 
   // this will Format MarkDown and JSON
-  '**/*.(json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.(json)': (filenames) => `pnpm prettier --write ${filenames.join(' ')}`,
 }
