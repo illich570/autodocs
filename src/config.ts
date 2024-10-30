e753bf26634dfff0d58591183d2d59ea65765aba
Illich Rada <illich570@gmail.com>
2024-10-30 14:39:53 -0400

    [ADD] config to url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const HOST_API = {
   dev: import.meta.env.VITE_HOST_API_URL,
-  production: 'https://api.example.com',
+  production: import.meta.env.VITE_HOST_API_URL,
 }
 
 const BASE_PATH = process.env.NODE_ENV === 'production' ? HOST_API.production : HOST_API.dev
